725c38824e71962991ffb66a6125c97f60ef3a6d
Patryk Janas <patryk.janas@nokia.com>
2022-10-04 15:02:28 +0200

    Created base page, Budget model and superuser

--- familybudgetapp/familybudget/models.py
+++ familybudgetapp/familybudget/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Budget(models.Model):
+    title = models.CharField(max_length=100)
+    amount_of_budget = models.IntegerField()
+    complete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
--- familybudgetapp/familybudget/views.py
+++ familybudgetapp/familybudget/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Budget
+
 
 # Create your views here.
+def index(request):
+    budgets = Budget.objects.all()
+    return render(request, 'family_budget_base.html', {'budget_list': budgets})
