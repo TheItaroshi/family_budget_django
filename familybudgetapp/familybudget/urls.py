803ea845a7d7b5cfd0aff1f910e492738ca619c2
Patryk Janas <patryk.janas@nokia.com>
2022-10-04 15:57:50 +0200

    Added basic functionalities, fixed some bugs (delete without request in view)

--- familybudgetapp/familybudget/urls.py
+++ familybudgetapp/familybudget/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('add', views.add, name='add'),
+    path('delete/<int:budget_id>/', views.delete, name='delete'),
+    path('add_to_budget/<int:budget_id>/', views.add_to_budget, name='add_to_budget'),
+    path('take_from_budget/<int:budget_id>/', views.take_from_budget, name='take_from_budget')
 ]
--- familybudgetapp/familybudget/views.py
+++ familybudgetapp/familybudget/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_http_methods
 from .models import Budget
 
 
@@ -6,3 +7,32 @@ from .models import Budget
 def index(request):
     budgets = Budget.objects.all()
     return render(request, 'family_budget_base.html', {'budget_list': budgets})
+
+
+@require_http_methods(['POST'])
+def add(request):
+    title = request.POST['title']
+    amount_of_budget = request.POST['amount_of_budget']
+    budget = Budget(title=title, amount_of_budget=amount_of_budget)
+    budget.save()
+    return redirect('index')
+
+
+def add_to_budget(request, budget_id):
+    budget = Budget.objects.get(id=budget_id)
+    budget.amount_of_budget += int(request.POST['add_to_budget'])
+    budget.save()
+    return redirect('index')
+
+
+def take_from_budget(request, budget_id):
+    budget = Budget.objects.get(id=budget_id)
+    budget.amount_of_budget -= int(request.POST['take_from_budget'])
+    budget.save()
+    return redirect('index')
+
+
+def delete(request, budget_id):
+    budget = Budget.objects.get(id=budget_id)
+    budget.delete()
+    return redirect('index')
